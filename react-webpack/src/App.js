270b08eec57dcc7f16c5138c63039205f00774fe
David Truxall <david.truxall@perspecta.com>
2019-10-16 14:48:49 -0400

    Added variable

--- react-webpack/src/App.js
+++ react-webpack/src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import logo from './logo.jpeg';
+import reactLogo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  let testPhrase = 'Not configured';
+
+  if (process.env.TEST_PHRASE !== undefined) {
+      testPhrase = process.env.TEST_PHRASE;
+  }
+
   return (
     <div className="App">
       <header className="header">
         <img src={logo} className="App-logo" alt="Configurator Logo" />
         <span>JavaScript Configurations for Everyone!</span>
-        <span className="placeholder"></span>
+        <img src={reactLogo} className="tech-logo" alt="React Logo" />
       </header>
       <div className="content">
-        Test Phrase:
+        Test Phrase: { testPhrase }
       </div>
     </div>
   );
