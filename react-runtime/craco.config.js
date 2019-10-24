c3b86d482aea26f4df764997777813a19d6485d1
David Truxall <david.truxall@perspecta.com>
2019-10-24 14:42:05 -0400

    Create new app for runtime config demo

---
+++ react-runtime/craco.config.js
@@ -0,0 +1,21 @@
+const { DefinePlugin } = require("webpack");
+
+let testPhrase = '';
+switch (process.env.NODE_ENV) {
+    case 'development':
+        testPhrase = '"I can do this all day"';
+        break;
+    case 'production':
+        testPhrase = JSON.stringify("Odin's Beard");
+        break;
+}
+
+module.exports = {
+    webpack: {
+        plugins: [
+            new DefinePlugin({
+                "process.env.TEST_PHRASE": testPhrase
+                })
+        ]
+    }
+};
---
+++ react-runtime/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.jpeg';
+import reactLogo from './logo.svg';
+
+import './App.css';
+
+function App() {
+  let testPhrase = 'Not configured';
+
+  if (process.env.TEST_PHRASE !== undefined) {
+      testPhrase = process.env.TEST_PHRASE;
+  }
+
+  return (
+    <div className="App">
+      <header className="header">
+        <div className="logo"><img src={logo} alt="Configurator Logo" /></div>
+        <div className="subtitle">JavaScript Configurations for Everyone!</div>
+        <div className="techlogo"><img src={reactLogo} alt="React Logo" /></div>
+      </header>
+      <div className="content">
+        Test Phrase: { testPhrase }
+      </div>
+    </div>
+  );
+}
+
+export default App;
