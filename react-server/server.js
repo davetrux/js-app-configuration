807c3acc9702bf7c6481ba1eb88da9a72dca367c
David Truxall <david.truxall@perspecta.com>
2019-10-29 14:25:30 -0400

    Added server-side configuration files

--- react-server/server.js
+++ react-server/server.js
@@ -3,21 +3,26 @@ const port = process.env.PORT || 3000;
 const path = require('path');
 const app = express();
 const fs = require('fs');
+const config = require('config');
 
 app.use(express.static(path.resolve(__dirname, './build')));
 
 app.get('*', function(request, response) {
   let filePath;
   if (request.originalUrl === '/') {
+    // This is the root file from React. Read it in to memory
     filePath = path.resolve(__dirname, './client/build', 'index.html');
     fs.readFile(filePath, 'utf8', function (err,data) {
       if (err) {
         return console.log(err);
       }
-      const result = data.replace('__SERVER_CONFIG__', JSON.stringify({ TEST_PHRASE: 'I can do this all day' }));
+      // Replace the placeholder text with actual configuration data
+      const result = data.replace('__SERVER_CONFIG__', JSON.stringify({ TEST_PHRASE: config.phrase }));
+
       response.send(result);
     });
   } else {
+    // All other files just get returned directly
     filePath = path.resolve(__dirname, './client/build' + request.originalUrl);
     response.sendFile(filePath);
   }
