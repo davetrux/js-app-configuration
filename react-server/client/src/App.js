f6b6ba9b4d7bc7d84d8f70ff0670ca44320f9742
David Truxall <david.truxall@perspecta.com>
2019-10-29 13:07:38 -0400

    Initial add

---
+++ react-server/client/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.jpeg';
+import reactLogo from './logo.svg';
+
+import './App.css';
+
+function App() {
+  let testPhrase = 'Not configured';
+  console.log(process.env)
+  if (process.env.REACT_APP_TEST_PHRASE !== undefined) {
+      testPhrase = process.env.REACT_APP_TEST_PHRASE;
+  }
+
+  return (
+    <div className="App">
+      <header className="header">
+        <div className="logo"><img src={logo} alt="Configurator Logo" /></div>
+        <div className="subtitle">CatchPhrase!</div>
+        <div className="techlogo"><img src={reactLogo} alt="React Logo" /></div>
+      </header>
+      <div className="content">
+        Test Phrase: { testPhrase }!
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const port = process.env.PORT || 3000;
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, './build')));
+
+app.get('*', function(request, response) {
+  const filePath = path.resolve(__dirname, './build', 'index.html');
+  response.sendFile(filePath);
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
